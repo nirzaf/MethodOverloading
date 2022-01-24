688c8b588bb871d00fa16edb2b4642490a4f1225
nirza <nirza@DESKTOP-A2VM3LC>
2022-01-24 22:03:08 +0530

    Add project files.

---
+++ MethodOverloading/MobilePhone.cs
@@ -0,0 +1,12 @@
+﻿public class MobilePhone
+{
+    public void Typing()
+    {
+        Console.WriteLine("Using Keypad");
+    }
+
+    public void Typing(bool isSmartPhone)
+    {
+        Console.WriteLine("Using QWERTY keyboard");
+    }
+}
---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Polymorphism using method overloading");
+
+MobilePhone mp = new MobilePhone();
+
+mp.Typing();
+
+mp.Typing(true);
+
+Console.ReadLine();
